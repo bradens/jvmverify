63750b36d93b2e2c7e999632f3795817863791c6
Braden Simpson <braden@uvic.ca>
2013-06-06 16:37:32 -0700

    changes

--- Test.java
+++ Test.java
@@ -1,3 +1,10 @@
+/*
+ * Test.java
+ * Braden Simpson (V00685500)
+ * Jordan Ell (V00660306)
+ * University of Victoria, CSC586A
+ * Virtual Machines
+ */
 public class Test {
 	public static int returnX() {
 		int x = 1;
